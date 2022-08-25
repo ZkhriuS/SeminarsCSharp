a32349ee60b580106bbc23b85118b91b8da5927c
Zakhar <seriy.zachar@yandex.by>
2022-08-26 01:00:39 +0300

    task 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.WriteLine("Enter 2 values: ");
+if (double.TryParse(Console.ReadLine(), out double first) && double.TryParse(Console.ReadLine(), out double second))
+if (first>second) Console.WriteLine($"max = {first}, min ={second}");
+else Console.WriteLine($"max = {second}, min ={first}");

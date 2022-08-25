28b31c0a2229bcf5de359f5a2b1d267240b457c4
Zakhar <seriy.zachar@yandex.by>
2022-08-26 01:11:54 +0300

    task 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter 3 values: ");
+if (double.TryParse(Console.ReadLine(), out double first) 
+    && double.TryParse(Console.ReadLine(), out double second)
+    && double.TryParse(Console.ReadLine(), out double third)){
+        double max =first;
+        if (second > max) max = second;
+        if (third > max) max = third;
+        Console.WriteLine($"max = {max}");
+}
+else{
+    Console.WriteLine("Incorrect input");
+}

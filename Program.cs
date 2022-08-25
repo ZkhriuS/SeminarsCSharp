26dd1b7fbe3ce401866ad52f573a59931bc42f45
Zakhar <seriy.zachar@yandex.by>
2022-08-26 01:46:56 +0300

    task 8

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a value: ");
+if (int.TryParse(Console.ReadLine(), out int first)){
+        int start = 1, step = 2;
+        if(first<start) {
+            start--;
+            step = -2;
+        }
+        else start++;
+        while(first - start > 1 || first - start < -1){
+           Console.Write(start+", ");
+           start+=step; 
+        }
+        Console.Write(start+" ");
+}
+else{
+    Console.WriteLine("Incorrect input");
+}

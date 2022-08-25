f0236932675b7973725abda3e59523cb78ab3e2d
Zakhar <seriy.zachar@yandex.by>
2022-08-26 01:49:11 +0300

    task 8

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿Console.WriteLine("Enter 2 values: ");
-if (double.TryParse(Console.ReadLine(), out double first) && double.TryParse(Console.ReadLine(), out double second))
+if (double.TryParse(Console.ReadLine(), out double first) && double.TryParse(Console.ReadLine(), out double second)){
 if (first>second) Console.WriteLine($"max = {first}, min ={second}");
 else Console.WriteLine($"max = {second}, min ={first}");
+}else{
+    Console.WriteLine("Incorrect input");
+}
